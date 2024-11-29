9e07d0fa07cb638960a85f2759a238f1259b436a
Ryo SUETSUGU <suetsugu.r@gmail.com>
2024-11-29 21:30:22 +0900

    add toggle test file command

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,32 @@
 import * as vscode from 'vscode';
+import { basename } from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
 
   console.log('Congratulations, your extension "vscode-quick-junit" is now active!');
 
-  const disposable = vscode.commands.registerCommand('vscode-quick-junit.helloWorld', () => {
-    vscode.window.showInformationMessage('Hello World from vscode-quick-junit!');
+  const disposable = vscode.commands.registerCommand('vscode-quick-junit.toggleTestFile', async () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      vscode.window.showInformationMessage('No file is currently open.');
+      return;
+    }
+
+    const currentFileName = basename(editor.document.uri.fsPath);
+    let targetFileName = null;
+    if (currentFileName.endsWith('Test.java')) {
+      targetFileName = currentFileName.replace(/Test\.java$/, '.java');
+    } else {
+      targetFileName = currentFileName.replace(/\.java$/, 'Test.java');
+    }
+
+    const files = await vscode.workspace.findFiles(`**/${targetFileName}`);
+    if (files.length === 0) {
+      return;
+    }
+
+    const document = await vscode.workspace.openTextDocument(files[0]);
+    await vscode.window.showTextDocument(document);
   });
 
   context.subscriptions.push(disposable);
